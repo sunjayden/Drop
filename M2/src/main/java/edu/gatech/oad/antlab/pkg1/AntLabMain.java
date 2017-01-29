f5384a714ec8a702ec0095edf1864aa16dbcf2b7
Jayden <jayden_sun@outlook.com>
2017-01-29 17:23:48 -0500

    added name

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -97,7 +97,7 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("Jayden");
+		  Person1 p1 = new Person1("Jayden Sun");
 		  toPrint += p1.toString("qsun46");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
@@ -109,7 +109,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("gburdell3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Mike");
+                  Person4 p4 = new Person4("Mike Mallow");
                   toPrint += p4.toString("gmallow3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
