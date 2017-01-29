7aa7a7ddde35ad10281148c45e0fbe03a98578b8
Zijia Ye <kiara.ye@hotmail.com>
2017-01-29 16:05:54 -0500

    p3 modify

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Zijia Ye
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,12 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	    String temp = "";
+	    int length = input.length();
+	    for (int i = length - 1; i >=0; i--) {
+	    	temp += input.charAt(i);
+	    }
+	    input = temp;
+	    return input;
 	}
 }
