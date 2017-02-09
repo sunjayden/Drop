f84745829ebb964ebaf4ac8cd109039ab4988c19
Jayden <jayden_sun@outlook.com>
2017-02-08 20:57:26 -0500

    file setup

---
+++ app/src/main/java/me/jaydensun/android/drop/controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package me.jaydensun.android.drop.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.jaydensun.android.drop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
