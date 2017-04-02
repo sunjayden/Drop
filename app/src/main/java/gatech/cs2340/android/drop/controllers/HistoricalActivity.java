3e1fccb545a1f8002a4b676f1ce6096fd2c0b820
bbbzhai <movingbob@hotmail.com>
2017-04-02 03:42:32 -0400

    dummy for historical report working

--- app/src/main/java/gatech/cs2340/android/drop/controllers/HistoricalActivity.java
+++ app/src/main/java/gatech/cs2340/android/drop/controllers/HistoricalActivity.java
@@ -1,16 +1,41 @@
 package gatech.cs2340.android.drop.controllers;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import gatech.cs2340.android.drop.R;
 
 public class HistoricalActivity extends AppCompatActivity {
+
+    private EditText _location;
+    private EditText _year;
+    private Spinner _typeSpinner;
+    private List legalType = Arrays.asList("Virus", "Contaminant");
+    private DatabaseReference mDatabasel;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +74,78 @@ public class HistoricalActivity extends AppCompatActivity {
             }
 
         });
+
+        // set spinner
+        _typeSpinner = (Spinner) findViewById(R.id.vSpinner);
+
+        // show in spinner
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter(this, R.layout.spinner_item, legalType);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        _typeSpinner.setAdapter(typeAdapter);
+
+        // change spinner triangle color to white
+        _typeSpinner.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.white),
+                PorterDuff.Mode.SRC.SRC_ATOP);
+
+
+        Button submitSp = (Button) findViewById(R.id.submit_his);
+        submitSp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHistory();
+            }
+        });
+
+
+
     }
+
+    private void showHistory() {
+        _location = (EditText) findViewById(R.id.editLocation);
+        _year = (EditText) findViewById(R.id.editYear);
+        final String location = _location.getText().toString().trim();
+        final String year = _year.getText().toString().trim();
+
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        //dummy example:
+        Calendar calendar = Calendar.getInstance();
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d2 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d3 = calendar.getTime();
+
+
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3)
+        });
+
+
+        // exmaple stops
+        graph.addSeries(series);
+
+
+
+
+        // set date label formatter
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(graph.getContext()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(d1.getTime());
+        graph.getViewport().setMaxX(d3.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+
+    }
+
+
+
+
+
 }
