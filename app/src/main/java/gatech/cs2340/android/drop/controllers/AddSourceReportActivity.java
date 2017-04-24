de2c354f0f871579163d8d8a1bca2297555201bf
Jayden <jayden_sun@outlook.com>
2017-04-24 00:00:38 -0400

    updated auto location

--- app/src/main/java/gatech/cs2340/android/drop/controllers/AddSourceReportActivity.java
+++ app/src/main/java/gatech/cs2340/android/drop/controllers/AddSourceReportActivity.java
@@ -1,8 +1,17 @@
 package gatech.cs2340.android.drop.controllers;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PorterDuff;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -43,16 +52,17 @@ public class AddSourceReportActivity extends AppCompatActivity {
     private Spinner _waterConditionSpinner;
     private DatabaseReference mDatabase;
     private FirebaseUser user;
+    String longitude;
+    String latitude;
+    LocationListener listener;
+    LocationManager locationManager;
+    Button getLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_source_report);
-        EditText lati = (EditText) findViewById(R.id.rp_lat);
-        lati.setText(getIntent().getStringExtra("latitude"));
-        EditText longi = (EditText) findViewById(R.id.rp_long);
-        longi.setText(getIntent().getStringExtra("longitude"));
-
 
         //Set Spinner
         _waterTypeSpinner = (Spinner) findViewById(R.id.water_type_spinner);
@@ -100,7 +110,80 @@ public class AddSourceReportActivity extends AppCompatActivity {
                 startActivity(cancelIntent);
             }
         });
+
+        getLocation = (Button) findViewById(R.id.getLocationButton);
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude =  Double.toString(location.getLatitude());
+                longitude = Double.toString(location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+
+
+                _lati = (EditText) findViewById(R.id.rp_lat);
+                _lati.setText("33.775618");
+                _long = (EditText) findViewById(R.id.rp_long);
+                _long.setText("-84.396285");
+
+            }
+        });
+    }
+
+
+
+
 
     private void createReport() {
         _lati = (EditText) findViewById(R.id.rp_lat);
